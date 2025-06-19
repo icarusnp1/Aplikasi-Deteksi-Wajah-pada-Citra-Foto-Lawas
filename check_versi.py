b6fee262cb53cfd8fa1012d21130d6d831a16030
Ragis Rahmatulloh <77946797+icarusnp1@users.noreply.github.com>
2025-06-19 10:04:41 +0700

    update check version

---
+++ check_versi.py
@@ -0,0 +1,9 @@
+import sys, cv2, numpy as np, matplotlib, dlib
+import PyQt5.QtCore
+
+print("Python     :", sys.version)
+print("OpenCV     :", cv2.__version__)
+print("NumPy      :", np.__version__)
+print("Matplotlib :", matplotlib.__version__)
+print("Dlib       :", dlib.__version__)
+print("PyQt5      :", PyQt5.QtCore.QT_VERSION_STR)
